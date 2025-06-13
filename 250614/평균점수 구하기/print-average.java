1f1e58ce211f65059898ecbfd8dc05f95478c90a
jiseom <76646494+jiseom@users.noreply.github.com>
2025-06-14 02:02:58 +0900

    Title: 평균점수 구하기, Time: 127ms, Memory: 12MB, Status: Wrong Answer - Codetree

---
+++ 250614/평균점수 구하기/print-average.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        
+    Scanner sc = new Scanner(System.in);
+    double total = 0.0;
+    int score = 8;
+    for (int i = 0; i < score; i++){
+        total += sc.nextDouble();
+    }
+        System.out.printf("%2.1f",(float)total / score);
+    }
+}
